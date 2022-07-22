c4fb35e9eadc2a59b8c9125f4a0fa385393532c1
chenyaguo <chenyaguo@jd.com>
2022-07-22 18:15:07 +0800

    frist commit

---
+++ woekbench-bascie-rpc/src/test/java/com/jdl/basic/rpc/WoekbenchBascieRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdl.basic.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoekbenchBascieRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workbench-basic-common/src/test/java/com/jdl/basic/dao/WorkbenchBasicDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdl.basic.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbenchBasicDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
