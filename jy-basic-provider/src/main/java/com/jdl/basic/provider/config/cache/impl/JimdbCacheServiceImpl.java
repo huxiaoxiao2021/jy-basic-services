bd88b56519499dbbdea321cc36a69759c6c22820
liuduo8 <liuduo3@jd.com>
2022-09-29 21:28:19 +0800

    fix:redis无效BUG修复

--- jy-basic-provider/src/main/java/com/jdl/basic/provider/config/cache/impl/JimdbCacheServiceImpl.java
+++ jy-basic-provider/src/main/java/com/jdl/basic/provider/config/cache/impl/JimdbCacheServiceImpl.java
@@ -8,6 +8,8 @@ import com.jdl.basic.provider.config.cache.CacheService;
 import org.codehaus.jackson.type.JavaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -29,7 +31,9 @@ public class JimdbCacheServiceImpl implements CacheService {
 	 */
 	private long exTime = 30*60;
 	private TimeUnit exTimeUnit = TimeUnit.SECONDS;
-	
+
+	@Qualifier("jimClient")
+	@Autowired
 	private Cluster jimdbClient;
     private static final byte EXPIRE_FLAG_VALUE = 1;
     /**
