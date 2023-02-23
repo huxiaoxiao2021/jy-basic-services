6a5cd5f9d98ee9bcfce0fdae7aa4c118c3cdf785
jinjingcheng <jinjingcheng@jd.com>
2023-02-23 19:50:14 +0800

    集包规则优化

--- jy-basic-provider/src/main/java/com/jdl/basic/provider/core/service/boxFlow/impl/CollectBoxFlowDirectionConfPushServiceImpl.java
+++ jy-basic-provider/src/main/java/com/jdl/basic/provider/core/service/boxFlow/impl/CollectBoxFlowDirectionConfPushServiceImpl.java
@@ -56,74 +56,78 @@ public class CollectBoxFlowDirectionConfPushServiceImpl  implements ICollectBoxF
     public Result<Void> updateOrNewConfig(CollectBoxFlowDirectionPushConfDto dto) {
         Result<Void> result = new Result<>();
         log.info("大数据更新集包规则:{}", JSONObject.toJSONString(dto));
+        try {
+            cluster.set(COLLECT_BOX_FLOW_DIRECTION_LASTEST_CONF_TIME, DateHelper.getDateOfyyMMddHHmmss(new Date()));
 
-        cluster.set(COLLECT_BOX_FLOW_DIRECTION_LASTEST_CONF_TIME, DateHelper.getDateOfyyMMddHHmmss(new Date()));
-
-        // 校验参数
-        if (dto.getStartSiteId() == null ||
-                dto.getEndSiteId() == null ||
-                StringUtils.isEmpty(dto.getStartSiteName()) ||
-                StringUtils.isEmpty(dto.getEndSiteName()) ||
-                StringUtils.isEmpty(dto.getBoxReceiveName()) ||
-                dto.getBoxReceiveId() == null ||
-                StringUtils.isEmpty(dto.getBoxPkgName()) ||
-                dto.getTransportType() == null ||
-                dto.getFlowType() == null ||
-                dto.getCollectClaim() == null ||
-                StringUtils.isEmpty(dto.getUpdateDate())) {
-            result.setCode(2);
-            result.setMessage("参数不能为空");
-            return result;
-        }
-        if (dto.getStartOrgId() == null) {
-            BaseStaffSiteOrgDto baseSiteBySiteId = basicPrimaryWS.getBaseSiteBySiteId(dto.getStartSiteId());
-            if (baseSiteBySiteId != null) {
-                dto.setStartOrgId(baseSiteBySiteId.getOrgId());
+            // 校验参数
+            if (dto.getStartSiteId() == null ||
+                    dto.getEndSiteId() == null ||
+                    StringUtils.isEmpty(dto.getStartSiteName()) ||
+                    StringUtils.isEmpty(dto.getEndSiteName()) ||
+                    StringUtils.isEmpty(dto.getBoxReceiveName()) ||
+                    dto.getBoxReceiveId() == null ||
+                    StringUtils.isEmpty(dto.getBoxPkgName()) ||
+                    dto.getTransportType() == null ||
+                    dto.getFlowType() == null ||
+                    dto.getCollectClaim() == null ||
+                    StringUtils.isEmpty(dto.getUpdateDate())) {
+                result.setCode(2);
+                result.setMessage("参数不能为空");
+                return result;
             }
-        }
-        if (dto.getEndOrgId() == null) {
-            BaseStaffSiteOrgDto baseSiteBySiteId = basicPrimaryWS.getBaseSiteBySiteId(dto.getEndSiteId());
-            if (baseSiteBySiteId != null) {
-                dto.setEndOrgId(baseSiteBySiteId.getOrgId());
+            if (dto.getStartOrgId() == null) {
+                BaseStaffSiteOrgDto baseSiteBySiteId = basicPrimaryWS.getBaseSiteBySiteId(dto.getStartSiteId());
+                if (baseSiteBySiteId != null) {
+                    dto.setStartOrgId(baseSiteBySiteId.getOrgId());
+                }
             }
+            if (dto.getEndOrgId() == null) {
+                BaseStaffSiteOrgDto baseSiteBySiteId = basicPrimaryWS.getBaseSiteBySiteId(dto.getEndSiteId());
+                if (baseSiteBySiteId != null) {
+                    dto.setEndOrgId(baseSiteBySiteId.getOrgId());
+                }
+            }
+
+            CollectBoxFlowDirectionConf conf = new CollectBoxFlowDirectionConf();
+            BeanUtils.copyProperties(dto, conf);
+            conf.setUpdateTime(new Date());
+
+            conf.setUpdateUserErp("大数据更新规则");
+            conf.setVersion(dto.getUpdateDate());
+            OrgEnum startOrgEnum = OrgEnum.getOrgEnum(dto.getStartOrgId());
+            OrgEnum endOrgEnum = OrgEnum.getOrgEnum(dto.getEndOrgId());
+
+            conf.setStartOrgName(startOrgEnum == null ? "" : startOrgEnum.getOrgName());
+            conf.setEndOrgName(endOrgEnum == null ? "" : endOrgEnum.getOrgName());
+
+            Result<CollectBoxFlowDirectionConf> verifyResult = verifyService.verifyBoxFlowDirectionConf(conf);
+            //  1.看是否更新了可混包和成品包 指定可混包
+            Integer originCollectClaim = verifyResult.getData() == null ? null : verifyResult.getData().getCollectClaim();
+            Integer collectClaim = conf.getCollectClaim();
+            //未更新
+            if (Objects.equals(originCollectClaim, collectClaim)) {
+                conf.setIfChangeSinceLastUpdate(0);
+            } else {
+                conf.setIfChangeSinceLastUpdate(conf.getCollectClaim());
+            }
+            conf.setCreateUserErp(verifyResult.getData() == null ? "大数据推送规则" : verifyResult.getData().getCreateUserErp());
+            conf.setYn(true);
+            //删除老版本数据
+            deleteAllOldVersion(dto.getUpdateDate());
+
+            Result<Boolean> booleanBaseEntity = confService.updateOrNewConfig(conf);
+            boolean success = booleanBaseEntity.isSuccess();
+            if (success) {
+                result.setCode(0);
+                result.setMessage("ok");
+            } else {
+                result.setCode(1);
+                result.setMessage(booleanBaseEntity.getMessage());
+            }
+        }catch (Exception e){
+            log.error("大数据更新集包规则异常",e);
         }
-
-        CollectBoxFlowDirectionConf conf = new CollectBoxFlowDirectionConf();
-        BeanUtils.copyProperties(dto, conf);
-        conf.setUpdateTime(new Date());
-
-        conf.setUpdateUserErp("大数据更新规则");
-        conf.setVersion(dto.getUpdateDate());
-        OrgEnum startOrgEnum = OrgEnum.getOrgEnum(dto.getStartOrgId());
-        OrgEnum endOrgEnum = OrgEnum.getOrgEnum(dto.getEndOrgId());
-
-        conf.setStartOrgName(startOrgEnum == null ? "" : startOrgEnum.getOrgName());
-        conf.setEndOrgName(endOrgEnum == null ? "" : endOrgEnum.getOrgName());
-
-        Result<CollectBoxFlowDirectionConf> verifyResult = verifyService.verifyBoxFlowDirectionConf(conf);
-        //  1.看是否更新了可混包和成品包 指定可混包
-        Integer originCollectClaim = verifyResult.getData() == null ? null : verifyResult.getData().getCollectClaim();
-        Integer collectClaim = conf.getCollectClaim();
-        //未更新
-        if (Objects.equals(originCollectClaim, collectClaim)) {
-            conf.setIfChangeSinceLastUpdate(0);
-        } else {
-            conf.setIfChangeSinceLastUpdate(conf.getCollectClaim());
-        }
-        conf.setCreateUserErp(verifyResult.getData() == null ? "大数据推送规则" : verifyResult.getData().getCreateUserErp());
-        conf.setYn(true);
-        //删除老版本数据
-        deleteAllOldVersion(dto.getUpdateDate());
-
-        Result<Boolean> booleanBaseEntity = confService.updateOrNewConfig(conf);
-        boolean success = booleanBaseEntity.isSuccess();
-        if (success) {
-            result.setCode(0);
-            result.setMessage("ok");
-        } else {
-            result.setCode(1);
-            result.setMessage(booleanBaseEntity.getMessage());
-        }
+        
         return result;
     }
     
