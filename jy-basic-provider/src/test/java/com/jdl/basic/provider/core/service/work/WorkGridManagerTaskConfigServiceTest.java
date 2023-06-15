ba04fb9e3e1e0b2d4e93e5cad2edcef2bc8767df
wuyoude <wuyoude@jd.com>
2023-06-15 15:40:16 +0800

    测试用例修改

--- jy-basic-provider/src/test/java/com/jdl/basic/provider/core/service/work/WorkGridManagerTaskConfigServiceTest.java
+++ jy-basic-provider/src/test/java/com/jdl/basic/provider/core/service/work/WorkGridManagerTaskConfigServiceTest.java
@@ -1,24 +1,24 @@
 package com.jdl.basic.provider.core.service.work;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.jdl.basic.provider.ApplicationLaunch;
-import com.jdl.basic.utils.EntityUtil;
-import com.jdl.basic.common.utils.JsonHelper;
 
-
-import com.jdl.basic.common.utils.Result;
-import com.jdl.basic.common.utils.PageDto;
 import com.jdl.basic.api.domain.work.WorkGridManagerTaskConfig;
 import com.jdl.basic.api.domain.work.WorkGridManagerTaskConfigQuery;
+import com.jdl.basic.api.domain.work.WorkGridManagerTaskConfigVo;
+import com.jdl.basic.common.utils.JsonHelper;
+import com.jdl.basic.common.utils.PageDto;
+import com.jdl.basic.common.utils.Result;
+import com.jdl.basic.provider.ApplicationLaunch;
+import com.jdl.basic.utils.EntityUtil;
 
 /**
  * 作业区巡检任务配置表--Service测试用例
@@ -39,18 +39,18 @@ public class WorkGridManagerTaskConfigServiceTest {
 
     @Test
     public void test(){
-    	WorkGridManagerTaskConfig insertData = EntityUtil.getInstance(WorkGridManagerTaskConfig.class);
+    	WorkGridManagerTaskConfigVo insertData = EntityUtil.getInstance(WorkGridManagerTaskConfigVo.class);
     	insertData.setYn(1);
     	Result<Boolean> insert = workGridManagerTaskConfigService.insert(insertData);
         Assert.assertTrue(insert != null && insert.getData());
         
         WorkGridManagerTaskConfigQuery query = new WorkGridManagerTaskConfigQuery();
-        Result<PageDto<WorkGridManagerTaskConfig>> queryPageList = workGridManagerTaskConfigService.queryPageList(query);
+        Result<PageDto<WorkGridManagerTaskConfigVo>> queryPageList = workGridManagerTaskConfigService.queryPageList(query);
         Assert.assertTrue(queryPageList != null && CollectionUtils.isNotEmpty(queryPageList.getData().getResult()));
         logger.info("queryPageList:{}"+JsonHelper.toJSONString(queryPageList));
         
-        WorkGridManagerTaskConfig updateData = queryPageList.getData().getResult().get(0);
-        Result<WorkGridManagerTaskConfig> queryById = workGridManagerTaskConfigService.queryById(updateData.getId());
+        WorkGridManagerTaskConfigVo updateData = queryPageList.getData().getResult().get(0);
+        Result<WorkGridManagerTaskConfigVo> queryById = workGridManagerTaskConfigService.queryById(updateData.getId());
         Assert.assertTrue(queryById != null && queryById.getData() != null);
         
         Result<Boolean> updateById = workGridManagerTaskConfigService.updateById(updateData);
@@ -59,7 +59,7 @@ public class WorkGridManagerTaskConfigServiceTest {
         Result<Boolean> deleteById = workGridManagerTaskConfigService.deleteById(updateData);
         Assert.assertTrue(deleteById != null && deleteById.getData());
         
-        Result<WorkGridManagerTaskConfig> queryByIdYn0 = workGridManagerTaskConfigService.queryById(updateData.getId());
+        Result<WorkGridManagerTaskConfigVo> queryByIdYn0 = workGridManagerTaskConfigService.queryById(updateData.getId());
         Assert.assertTrue(queryByIdYn0 != null && queryByIdYn0.getData() == null);
     }
 
