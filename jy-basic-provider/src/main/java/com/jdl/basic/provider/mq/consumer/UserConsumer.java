2dad5dca31a05287074e704ad776a346cd262430
weixiaofeng12 <weixiaofeng12@jd.com>
2023-06-25 09:07:45 +0800

    更新用户所属场地数据

--- jy-basic-provider/src/main/java/com/jdl/basic/provider/mq/consumer/UserConsumer.java
+++ jy-basic-provider/src/main/java/com/jdl/basic/provider/mq/consumer/UserConsumer.java
@@ -4,8 +4,10 @@ import static com.jdl.basic.common.contants.Constants.LOCK_EXPIRE;
 
 import com.jd.jmq.common.message.Message;
 import com.jd.joyqueue.client.springboot2.annotation.JmqListener;
+import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
 import com.jdl.basic.api.domain.user.JyUser;
 import com.jdl.basic.api.enums.UserSatusEnum;
+import com.jdl.basic.api.enums.WorkSiteTypeEnum;
 import com.jdl.basic.common.contants.Constants;
 import com.jdl.basic.common.utils.BeanUtils;
 import com.jdl.basic.common.utils.DateHelper;
@@ -63,6 +65,17 @@ public class UserConsumer {
         }
         try {
           JyUser exitUser = userService.queryUserInfo(condition);
+          BaseStaffSiteOrgDto baseStaffSiteOrgDto =baseMajorManager.getBaseStaffByErp(userInfo.getUserName());
+          if (ObjectHelper.isNotNull(baseStaffSiteOrgDto)){
+            condition.setSiteCode(baseStaffSiteOrgDto.getSiteCode());
+            condition.setSiteName(baseStaffSiteOrgDto.getSiteName());
+            WorkSiteTypeEnum siteTypeEnum =WorkSiteTypeEnum.getWorkingSiteTypeBySubType(baseStaffSiteOrgDto.getSubType());
+            if (ObjectHelper.isEmpty(siteTypeEnum)){
+              log.info("非分拣中心类型场地数据：{}",content);
+              return;
+            }
+            condition.setSiteType(siteTypeEnum.getCode());
+          }
           int rs = 0;
           Date now = new Date();
           if (ObjectHelper.isNotNull(exitUser)) {
