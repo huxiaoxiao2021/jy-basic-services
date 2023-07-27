52191817d550efebe61e6fd28ed458056b9dcffa
laoqingchang1 <laoqingchang1@jd.com>
2023-07-27 16:40:16 +0800

    添加工种映射关系

--- jy-basic-api/src/main/java/com/jdl/basic/api/enums/JyJobTypeEnum.java
+++ jy-basic-api/src/main/java/com/jdl/basic/api/enums/JyJobTypeEnum.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public enum JyJobTypeEnum {
-  FULL_TIME_LABORER(1,"正式工", Arrays.asList("A", "D", "L")),
-  LABOR_DISPATCHER(2,"派遣工", Arrays.asList("E")),
+  FULL_TIME_LABORER(1,"正式工", Arrays.asList("A", "D", "L", "H", "U", "T")),
+  LABOR_DISPATCHER(2,"派遣工", Arrays.asList("E", "F", "K")),
   LABOR_OUTSOURCING(3,"外包工", Arrays.asList("B", "P")),
   NO_FULL_TIME_LABORER(4,"临时工", Arrays.asList("G", "I")),
 
