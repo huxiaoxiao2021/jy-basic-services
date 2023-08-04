693d1fd9215197748391b2b6988305eb8d21ec88
weixiaofeng12 <weixiaofeng12@jd.com>
2023-08-04 15:36:24 +0800

    提供查询拣运场地接口

--- jy-basic-provider/src/main/java/com/jdl/basic/provider/core/manager/basicSiteQueryWS/Impl/IBasicSiteQueryWSManagerImpl.java
+++ jy-basic-provider/src/main/java/com/jdl/basic/provider/core/manager/basicSiteQueryWS/Impl/IBasicSiteQueryWSManagerImpl.java
@@ -1,11 +1,17 @@
 package com.jdl.basic.provider.core.manager.basicSiteQueryWS.Impl;
 
+import com.jd.ql.basic.domain.BaseSite;
 import com.jd.ql.basic.dto.BaseSiteInfoDto;
+import com.jd.ql.basic.dto.BaseSiteSimpleDto;
+import com.jd.ql.basic.dto.PageDto;
 import com.jd.ql.basic.ws.BasicSiteQueryWS;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
+import com.jd.ump.profiler.CallerInfo;
+import com.jd.ump.profiler.proxy.Profiler;
 import com.jdl.basic.common.contants.Constants;
 import com.jdl.basic.provider.core.manager.basicSiteQueryWS.IBasicSiteQueryWSManager;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +23,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class IBasicSiteQueryWSManagerImpl implements IBasicSiteQueryWSManager {
-    
+
     @Autowired
     private BasicSiteQueryWS basicSiteQueryWS;
-    
+
     @Override
     @JProfiler(jKey = Constants.UMP_APP_NAME + ".IBasicSiteQueryWSManagerImpl.getBaseSiteInfoBySiteId", jAppName=Constants.UMP_APP_NAME, mState={JProEnum.TP,JProEnum.FunctionError})
     public BaseSiteInfoDto getBaseSiteInfoBySiteId(Integer siteCode) {
@@ -28,6 +34,26 @@ public class IBasicSiteQueryWSManagerImpl implements IBasicSiteQueryWSManager {
             return basicSiteQueryWS.getBaseSiteInfoBySiteId(siteCode);
         }catch (Exception e){
             log.error("根据网点ID：{}查询网点信息异常!", siteCode);
+        }
+        return null;
+    }
+
+    @Override
+    public PageDto<List<BaseSiteSimpleDto>> querySiteByCondition(BaseSite site, PageDto pageDto) {
+        CallerInfo info = Profiler.registerInfo("com.jdl.basic.provider.core.manager.IBasicSiteQueryWSManager.querySiteByCondition",
+            Constants.UMP_APP_NAME, false, true);
+        try{
+            if(pageDto == null || pageDto.getCurPage() <= Constants.NUMBER_ZERO || pageDto.getPageSize() <= Constants.NUMBER_ZERO){
+                pageDto = new PageDto<>();
+                pageDto.setCurPage(Constants.CONSTANT_NUMBER_ONE);
+                pageDto.setPageSize(Constants.CONSTANT_NUMBER_TEN);
+            }
+            return basicSiteQueryWS.querySiteByCondition(site, pageDto);
+        }catch (Exception e){
+            log.error("querySiteByCondition分页查询站点异常!", e);
+            Profiler.functionError(info);
+        }finally {
+            Profiler.registerInfoEnd(info);
         }
         return null;
     }
--- jy-basic-provider/src/main/java/com/jdl/basic/provider/core/service/site/BaseSiteQueryServiceImpl.java
+++ jy-basic-provider/src/main/java/com/jdl/basic/provider/core/service/site/BaseSiteQueryServiceImpl.java
@@ -1,14 +1,32 @@
 package com.jdl.basic.provider.core.service.site;
 
 import com.jd.etms.framework.utils.cache.annotation.Cache;
+import com.jd.ql.basic.domain.BaseSite;
+import com.jd.ql.basic.domain.PsStoreInfo;
+import com.jd.ql.basic.dto.BaseSiteSimpleDto;
+import com.jd.ql.basic.dto.PageDto;
+import com.jd.ql.basic.util.PageUtil;
+import com.jd.ump.profiler.proxy.Profiler;
 import com.jdl.basic.api.dto.site.AreaVO;
+import com.jdl.basic.api.dto.site.BasicSiteVO;
 import com.jdl.basic.api.dto.site.ProvinceAgencyVO;
+import com.jdl.basic.api.dto.site.SiteQueryCondition;
+import com.jdl.basic.api.enums.WorkSiteTypeEnum;
 import com.jdl.basic.api.service.site.SiteQueryService;
+import com.jdl.basic.common.contants.Constants;
+import com.jdl.basic.common.utils.JsonHelper;
+import com.jdl.basic.common.utils.ObjectHelper;
+import com.jdl.basic.common.utils.Pager;
 import com.jdl.basic.common.utils.Result;
 import com.jdl.basic.provider.core.enums.BasicAreaEnum;
 import com.jdl.basic.provider.core.enums.BasicProvinceAgencyEnum;
+import com.jdl.basic.provider.core.manager.basicSiteQueryWS.IBasicSiteQueryWSManager;
+import com.jdl.basic.rpc.exception.JYBasicRpcException;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,6 +37,8 @@ public class BaseSiteQueryServiceImpl implements SiteQueryService {
 
 
 
+    @Autowired
+    private IBasicSiteQueryWSManager basicSiteQueryWSManager;
 
 
     @Cache(key = "SiteQueryService.queryAllProvinceAgencyInfo", memoryEnable = true, memoryExpiredTime = 30 * 60 * 1000
@@ -89,5 +109,79 @@ public class BaseSiteQueryServiceImpl implements SiteQueryService {
             result.toFail("未查询到枢纽信息!");
         }
         return result;
+    }
+
+    @Override
+    public Result<Pager<BasicSiteVO>> queryJySiteByConditionFromBasicSite(Pager<SiteQueryCondition> siteQueryPager) {
+        checkSiteQueryPager(siteQueryPager);
+        Result<Pager<BasicSiteVO>> result = assemblePageResult(siteQueryPager);
+        if(checkIsQueryJySite(siteQueryPager.getSearchVo().getSubTypes())){
+            PageDto<Object> pageDto = new PageDto<>();
+            pageDto.setCurPage(siteQueryPager.getPageNo());
+            pageDto.setPageSize(siteQueryPager.getPageSize());
+            PageDto<List<BaseSiteSimpleDto>> pageSiteResult = basicSiteQueryWSManager.querySiteByCondition(convertSiteQuery(siteQueryPager.getSearchVo()), pageDto);
+            if(pageSiteResult != null && CollectionUtils.isNotEmpty(pageSiteResult.getData())){
+                result.getData().setTotal((long) pageSiteResult.getTotalRow());
+                result.getData().getData().addAll(convertBasicSiteList(pageSiteResult.getData()));
+            }
+        }
+        return result;
+    }
+
+    private List<BasicSiteVO> convertBasicSiteList(List<BaseSiteSimpleDto> data) {
+        return data.stream().map(baseSiteSimpleDto -> {
+            BasicSiteVO basicSiteVO =new BasicSiteVO();
+            basicSiteVO.setSiteCode(baseSiteSimpleDto.getSiteCode());
+            basicSiteVO.setSiteName(baseSiteSimpleDto.getSiteName());
+            //basicSiteVO.setProvinceAgencyCode(baseSiteSimpleDto.getProvinceAgencyCode());
+            //basicSiteVO.setProvinceAgencyName(baseSiteSimpleDto.getProvinceAgencyName());
+            return basicSiteVO;
+        }).collect(Collectors.toList());
+    }
+
+    private Result<Pager<BasicSiteVO>> assemblePageResult(Pager<SiteQueryCondition> siteQueryPager) {
+        Result result =new Result();
+        Pager<BasicSiteVO> pager =new Pager();
+        pager.setPageNo(siteQueryPager.getPageNo());
+        pager.setPageSize(siteQueryPager.getPageSize());
+        List<BasicSiteVO> basicSiteVOList =new ArrayList<>();
+        pager.setData(basicSiteVOList);
+        result.setData(pager);
+        return result;
+    }
+
+    private BaseSite convertSiteQuery(SiteQueryCondition siteQueryCondition) {
+        BaseSite siteQuery = new BaseSite();
+        siteQuery.setSubTypeList(siteQueryCondition.getSubTypes());
+        return siteQuery;
+    }
+
+    private boolean checkIsQueryJySite(List<Integer> subTypes) {
+        if (CollectionUtils.isNotEmpty(subTypes)){
+            for (Integer subType: subTypes){
+                if (null ==WorkSiteTypeEnum.getWorkingSiteTypeBySubType(subType)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void checkSiteQueryPager(Pager<SiteQueryCondition> siteQueryPager) {
+        if (ObjectHelper.isEmpty(siteQueryPager.getPageNo())){
+            throw  new JYBasicRpcException("参数异常：缺失分页页码！");
+        }
+        if (ObjectHelper.isEmpty(siteQueryPager.getPageSize())){
+            throw  new JYBasicRpcException("参数异常：缺失分页数量！");
+        }
+        if (ObjectHelper.isEmpty(siteQueryPager.getSearchVo())){
+            throw  new JYBasicRpcException("参数异常：缺失查询条件！");
+        }
+        if (CollectionUtils.isEmpty(siteQueryPager.getSearchVo().getSubTypes())){
+            throw  new JYBasicRpcException("参数异常：缺失子站点类型！");
+        }
+        if (siteQueryPager.getPageSize()> Constants.DEFAULT_PAGE_SIZE_QUERY_USER){
+            throw  new JYBasicRpcException("参数异常：查询数量过大！");
+        }
     }
 }
