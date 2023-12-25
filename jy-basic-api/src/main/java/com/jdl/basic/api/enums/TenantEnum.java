10dff27ea548415cb4a90d54b45df8e7c24aa011
caozhixing3 <caozhixing3@jd.com>
2023-12-25 15:56:31 +0800

    feat(数据隔离): 添加方法

--- jy-basic-api/src/main/java/com/jdl/basic/api/enums/TenantEnum.java
+++ jy-basic-api/src/main/java/com/jdl/basic/api/enums/TenantEnum.java
@@ -1,5 +1,7 @@
 package com.jdl.basic.api.enums;
 
+import com.jdl.basic.common.utils.StringUtils;
+
 /**
  * @author : caozhixing3
  * @version V1.0
@@ -36,5 +38,22 @@ public enum TenantEnum {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * 获取具有默认值的租户枚举。
+     * @param code 入参，要匹配的租户代码
+     * @return 匹配的租户枚举，如果没有匹配则返回默认枚举
+     */
+    public static TenantEnum getTenantEnumWithDefault(String code) {
+        if (StringUtils.isBlank(code)) {
+            return TenantEnum.TENANT_JY;
+        }
+        for (TenantEnum currEnum : TenantEnum.values()) {
+            if (code.equals(currEnum.getCode())) {
+                return currEnum;
+            }
+        }
+        return TenantEnum.TENANT_JY;
     }
 }
